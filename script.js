d2dfabac1186a240f9f48ccc686f7cb4c18fc093
Akshaymakhija <98867421+Akshaymakhija@users.noreply.github.com>
2024-02-27 01:25:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* *********************** typing animation ********************/ 
+var typed = new Typed(".typing", {
+strings: [ "Python Developer", "Web Developer", "Graphic Designer"],
+typeSpeed: 150,
+backSpeed: 60,
+loop: true,
+});
